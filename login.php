ec05516f31994305094cd804d52fb931b88d8819
kjesvale <kjesvale@hotmail.com>
2014-09-28 18:06:35 -0700

    New login

--- login.php
+++ login.php
@@ -1,9 +1,5 @@
 <?php
-
-if ($_POST && !empty($_POST['username']) && !empty($_POST['pwd'])) {
-	validateLogin($_POST['username'], $_POST['pwd']);
-}
-
+session_start();
 ?>
 
 <!DOCTYPE html>
@@ -15,19 +11,24 @@ if ($_POST && !empty($_POST['username']) && !empty($_POST['pwd'])) {
 	<div id = "login">
 		<h2>Login</h2>
 
-		<form method="POST" action="">
+		<form method="POST" action="loginAction.php">
 			<p>
 				<label for="name">Username: </label>
 				<input type="text" id="name" name="username" autofocus="true"/>
 			</p>
 			<p>
 				<label for="pwd">Password: </label>
-				<input type="password" id="pwd" name="pwd" />
+				<input type="password" id="pwd" name="password" />
+			</p>
 			<p>
 				<input type="submit" value="Login" name="submit" />
 			</p>
-		</form>
 
+			<?php
+				if (isset($_SESSION['login_message']))
+					echo $_SESSION['login_message'];
+			?>
+		</form>
 	</div>
 </body>
 </html>
