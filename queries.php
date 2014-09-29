21b691a2bc32cf6545ffb75dd2375e746ae889ee
kjesvale <kjesvale@hotmail.com>
2014-09-28 18:00:29 -0700

    Added the login- and logout functions. Merged DatabaseFunctions.php and queries.php into one file, see "queries.php". This communicates with the constants.php" file.

--- queries.php
+++ queries.php
@@ -1,16 +1,14 @@
 <?php
 
 /*
- * @Author Kjetil Svalestuen
- * 
- * Contains MySQL queries used to access the userinfo-database.
+ * Includes all the functions from "DatabaseFunctions.php" and
+ * "queries.php".
  */
 
 require_once 'constants.php';
-$con = connectionOpen();
+$con = connect();
 
-/** Returns a connection to MySQL. */
-function connectionOpen() {
+function connect() {
 	return mysqli_connect(DB_SERVER, DB_USER, DB_PASSWORD, DB_NAME);
 }
 
@@ -20,7 +18,7 @@ function connectionOpen() {
  * @return true if the connection was successful
  * @return false if the connection was not made
  */
-function connectionValid() {
+function checkConnection() {
 	if (mysqli_connect_errno()) {
 		echo "Failed to connect to MySQL: " . mysqli_connect_error();
 		return false;
@@ -28,9 +26,10 @@ function connectionValid() {
 }
 
 /** 
- * Returns 'true' if the given username is available, 'false' if it is already taken.
+ * Checks a username's availability.
  * 
  * @param String username the chosen username of the user
+ * @return 'true' if the given username is available, 'false' if not.
  */
 function usernameAvailable($username) {
 	global $con;
@@ -47,22 +46,39 @@ function usernameAvailable($username) {
 		return true;
 }
 
-/** 
- * Adds a new user to the database
- * 
- * @param String user the chosen username of the user
- * @param String name the real name of the user
- * @param String password the password the user has chosen
- */
-function insertUser($user, $name, $password) {
-	global $con;
-	
-	mysqli_query($con, 
-		"INSERT INTO userinfo (username, realname, password)
-		 VALUES ('$user', '$name', '$password')"
-	);
+/**
+  * Function used to input user into database with all the information
+  *
+  * takes the information from the registration page and inputs it into the database.
+  *
+  * @param String $uName is the username of the user whos information will be put in.
+  * @param String $password is the password of the user whos information will be put in.
+  * @param String $name is the email of the user whos information will be put in.
+  */
+function register($uName, $password, $name)
+{
+		global $con;	
 
-	return true;
+	mysqli_query($con,"insert into userInfo (username, realName, password) values('$uName', '$name', '$password');");
+	return TRUE;    
+}
+
+function getPassword($uName)
+{
+		global $con;
+	
+	$result = mysqli_query($con,"select password from userInfo WHERE username='$uName'");
+	$resultArray = mysqli_fetch_array($result);
+	return $resultArray[0];    
+}
+
+function getName($uName)
+{
+		global $con;
+		
+	$result = mysqli_query($con,"select realName from userInfo WHERE username='$uName'");
+	$resultArray = mysqli_fetch_array($result);
+	return $resultArray[0];   
 }
 
 /** 
@@ -75,12 +91,25 @@ function validateLogin($username, $password) {
 	global $con;
 
 	$query = "SELECT * FROM userinfo
-		WHERE username = '$username'
-		AND password = '$password'";
+		WHERE username = ?
+		AND password = ?
+		LIMIT 1";
 
-	$result = mysqli_query($con, $query);
-	if (mysqli_num_rows($result) > 0)
-		return true;
-	else
-		return false;
+	if ($statement = $con->prepare($query)) {
+		$statement->bind_param('ss', $username, $password);
+		$statement->execute();
+
+		if ($statement->fetch()) {
+			$statement->close();
+			return true;
+		} else return false;
+	}
 }
+
+/*
+ * ADD NEW DATABASE/QUERY FUNCTIONS HERE.
+ */
+
+
+
+?>
