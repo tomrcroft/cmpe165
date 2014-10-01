517e16833cd3864fcbddb31d16ed7a61db40d568
artbtjr <artbtjr@gmail.com>
2014-09-30 19:42:47 -0700

    Front End without Login, Register, Logout connectors

---
+++ squadfree/logout.php
@@ -0,0 +1,7 @@
+<?php   
+session_start(); //to ensure you are using same session
+$_SESSION = array(); // Unset all of the session variables.
+session_destroy(); //destroy the session
+header("location:/index.php"); //to redirect back to "index.php" after logging out
+exit();
+?>
