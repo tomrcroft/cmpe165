e5353ff0fca177543d6af02a50b8f111ae813810
artbtjr <artbtjr@gmail.com>
2014-11-06 14:46:49 -0800

    ~

--- viewpinmodal.php
+++ viewpinmodal.php
@@ -33,9 +33,7 @@
                         </div>
 
                         <div class="form-group">
-                            <div class="col-md-9">
-                                <img src=<?php echo $localImgLink ?> />
-                            </div>
+                            <img style="height:auto; max-width:560px;" src=<?php echo $localImgLink ?> />
                         </div>
 
                         <form id="commentform" class="form-horizontal" name="commentform" action="" method="POST">
