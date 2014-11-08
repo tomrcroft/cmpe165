1a08992e6cb79984450e7c9fd2bc3d53d289cf8d
mmilanovich <mmilanovich@gmail.com>
2014-11-08 13:01:15 -0800

    Merge branch 'master' of https://github.com/tomrcroft/cmpe165
    Conflicts:
    	viewpinmodal.php

--- pins.php
+++ pins.php
@@ -64,7 +64,7 @@
 	<!-- This is called when a pin is clicked, passes image link and pin title-->
 	<script type="text/javascript">
 		$(document).on("click", ".open-viewPin", function () {
-			 var src =$(this).data('id');
+			 var src =$(this).data('link');
 			 var title = $(this).data('title');
 			 // passes the correct image link to viewpinmodal
 		     $(".showPic").attr("src", src);
@@ -116,7 +116,7 @@
                 for($i = 0; $i < count($pins); $i++) {
 					//$_SESSION['imgLink'] = $pins[$i];
                     echo '
-                        <a href="#viewPin" data-target="#viewPin" data-toggle= "modal" class="open-viewPin" data-id="'.$pins[$i].'" data-title="'.$pinNames[$i].'">
+                        <a href="#viewPin" data-target="#viewPin" data-toggle= "modal" class="open-viewPin" data-link="'.$pins[$i].'" data-title="'.$pinNames[$i].'">
                             <div class="item">
                                 <img src="'.$pins[$i].'" />
                             </div>';
