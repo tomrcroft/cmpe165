7f76714544aaabc91fb728d0a0339a38918e9995
ardalan <razavi.ardalan@gmail.com>
2014-11-12 00:50:21 -0800

    Sending Email Confirmation for new users
    Sending and Confirmation PHP
    needing some changes to DB
    
    need new table to insert the temp members info
    need email column in both TABLES

---
+++ confirmationCheck.php
@@ -0,0 +1,49 @@
+<?php
+
+//Ardalan Razavi
+
+
+$passkey=$_GET['passkey'];
+$temp_table="temp_members_table";
+$registered_member_table="registered_members";
+
+$query="SELECT * FROM $temp_table WHERE confirm_code ='$passkey'";
+$result1=mysql_query($query);
+
+
+if($result1){
+
+
+$count=mysql_num_rows($result1);
+
+if($count==1){
+$rows=mysql_fetch_array($result1);
+$name=$rows['name'];
+$email=$rows['email'];
+$password=$rows['password']; 
+$username=$rows['username'];
+$question =$rows['question'];
+$answer =$rows['answer'];
+
+$queryInset="INSERT INTO $registered_member_table(name, email, password, username,question,answer)VALUES('$name', '$email', '$password', '$username','$question','$answer')";
+$result2=mysql_query($queryInsert);
+}
+
+
+else {
+echo "Confirmation code is not correct";
+}
+
+if($result2){
+
+echo "Wohoo, you are actual member of our site, congratulation";
+
+
+ 
+$queryDelete="DELETE FROM $temp_table WHERE confirm_code = '$passkey'";
+$result3=mysql_query($queryDelete);
+
+}
+
+}
+?>
