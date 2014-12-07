e7dd27878f82fc9b5c710f42376dbd8360a0849c
mmilanovich <mmilanovich@gmail.com>
2014-12-07 15:51:06 -0800

    formatting

--- boards.php
+++ boards.php
@@ -76,7 +76,7 @@
 			 var commentAuthors = $(this).attr('data-commentAuthors').split("~");
 			 var desription = $(this).attr('data-pinDescription');
 			 var pinLikes = $(this).attr('data-pinLikes');
-			 $('.pinLikesBadge').html('<li class="active" style="padding-left:3em">Likes<span class="badge pull-right">' + pinLikes + '</span></li>');
+			 $('.pinLikesBadge').html('<li class="active">Likes<span class="badge pull-right">' + pinLikes + '</span></li>');
 			 $(".oldPinName").val(title);
 			 $(".newPinName").val(title);
 			 $(".pinDescription").val(desription);
