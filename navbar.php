0d2e2e1b03d7c3dd042d9a477b18acc68f4069df
artbtjr <artbtjr@gmail.com>
2014-12-08 02:33:50 -0800

    undid navbar

--- navbar.php
+++ navbar.php
@@ -18,9 +18,20 @@
 
 								 if (!$verify)
 							 {
+							 
+							 echo ' <div class="alert alert-danger" role="alert">
+								<span class="glyphicon glyphicon-exclamation-sign" aria-hidden="true"></span>
+								<span class="sr-only">Error:</span>
+								Please verify your email address. 
+								<iframe style="display:none;" name="target"></iframe>
+								<a href="resendverification.php" target="target"> Resend Verification</a>
+								</div>';
+							 
+							 }
 							
 							
-                            echo '<li>
+                            echo '<li class="dropdown">
+
                             		<div class="alert alert-danger" role="alert">
 										<span class="glyphicon glyphicon-exclamation-sign" aria-hidden="true"></span>
 										<span class="sr-only">Error:</span>
@@ -28,8 +39,6 @@
 										<iframe style="display:none;" name="target"></iframe>
 										<a href="resendverification.php" target="target"> Resend Verification</a>
 									</div>
-								</li>
-                            	<li class="dropdown">
 								        <a href="#" class="dropdown-toggle" data-toggle="dropdown">'.$user.'<b class="caret"></b></a>
 							        <ul class="dropdown-menu">
 								        <li><a href="myBoards.php?username='.$_SESSION['username'].'">My Boards</a></li>
